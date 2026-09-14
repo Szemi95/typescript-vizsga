0da5b40dac1c3b40e322355cf211447bcd6b990c
szemi95 <sziemju@gmail.com>
2026-09-14 20:21:28 +0200

    Remove generated JavaScript files

--- src/utils/logClass.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LogClass = LogClass;
-function LogClass(constructor) {
-    console.log(`Az osztály neve: ${constructor.name}`);
-}
