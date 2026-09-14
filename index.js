0029c9fbc5d09d7536d8a1a3fd7803b01340c012
szemi95 <sziemju@gmail.com>
2026-09-14 18:48:31 +0200

    Remove unnecessary files

--- index.js
+++
@@ -1,3 +0,0 @@
-import { Event } from "./src/models/Event.js";
-const event = new Event(1, "Születésnap", "Budapest", new Date("2026-11-11"), ["Teszt Elekné", "Teszt Elek", "Gipsz Jakab"], "családi");
-console.log(event);
